d9b8574a632a87fb7ab76c6d9ffc5657a4ce54c2
EmmaH2012 <emmakatherinehoward@icloud.com>
2026-07-04 13:24:11 -0400

    Update code.py

--- code.py
+++ code.py
@@ -11,8 +11,10 @@ if problem == "anxiety":
         "by prayer and petition with thanksgiving, present your requests to God.' "
         "- Philippians 4:6"
     ) choice = input("Would you like a Bible study about anxiety? (please type Y or N): ").strip().lower()
-    if choice == Y print('Sorry, this feature isnt avalible yet please try again later!')
-if choice == N print('Ok! I hope this helped!')
+    if choice == "Y":
+         print('Sorry, this feature isnt avalible yet please try again later!')
+elif choice == "N":
+     print('Ok! I hope this helped!')
 else:
     print("Sorry, I don't have a verse for that topic yet! Please try again later or check spelling.")
 
